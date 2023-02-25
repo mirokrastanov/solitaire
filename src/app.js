f938f51f0fc73c9353c5e4ec6295bd3c8c6e0f38
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-02-25 17:33:20 +0200

    State utilization established

--- src/app.js
+++ src/app.js
@@ -9,31 +9,11 @@ import {
     Foundation,
     Pile,
 } from './cards.js';
-import { createDeck, shuffleDeck } from './util.js';
+import { createDeck, shuffleDeck, dealDeck } from './util.js';
 
-start();
 
 function start() {
-    const state = {
-        stock: new Stock(),
-        waste: new Waste(),
-        foundations: {
-            [suits.Clubs]: new Foundation([], suits.Clubs),
-            [suits.Diamonds]: new Foundation([], suits.Diamonds),
-            [suits.Hearts]: new Foundation([], suits.Hearts),
-            [suits.Spades]: new Foundation([], suits.Spades),
-        },
-        piles: [
-            new Pile(),
-            new Pile(),
-            new Pile(),
-            new Pile(),
-            new Pile(),
-            new Pile(),
-            new Pile(),
-        ],
-    };
-    console.log(state);
+    
     const deck = createDeck();
     console.log(deck);
     shuffleDeck(deck);
@@ -42,4 +22,8 @@ function start() {
     shuffleDeck(deck);
     shuffleDeck(deck);
     console.log(deck);
+    
+    const state = dealDeck(deck);
+    console.log(state);
 }
+start();
--- src/util.js
+++ src/util.js
@@ -34,7 +34,48 @@ function shuffleDeck(deck) {
     deck.cards.push(...stock);
 }
 
+/**
+ * 
+ * @param {Deck} deck
+ * @returns {GameState} 
+ */
+function dealDeck(deck) {
+    const state = {
+        stock: new Stock(),
+        waste: new Waste(),
+        foundations: {
+            [suits.Clubs]: new Foundation([], suits.Clubs),
+            [suits.Diamonds]: new Foundation([], suits.Diamonds),
+            [suits.Hearts]: new Foundation([], suits.Hearts),
+            [suits.Spades]: new Foundation([], suits.Spades),
+        },
+        piles: [
+            new Pile(),
+            new Pile(),
+            new Pile(),
+            new Pile(),
+            new Pile(),
+            new Pile(),
+            new Pile(),
+        ],
+    };
+}
+
+/**
+ * @typedef {Object} GameState
+ * @property {Stock} stock
+ * @property {Waste} waste
+ * @property {Object} foundations
+ * @property {Foundation} foundations.clubs
+ * @property {Foundation} foundations.diamonds
+ * @property {Foundation} foundations.hearts
+ * @property {Foundation} foundations.spades
+ * @property {[Pile, Pile, Pile, Pile, Pile, Pile, Pile]} piles
+ */
+
+
 export {
     createDeck,
     shuffleDeck,
+    dealDeck,
 }
